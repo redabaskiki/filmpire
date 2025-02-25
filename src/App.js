da0f856df91e7bf9c7f8385935ca8c84298d87d8
reda baskiki <baskikireda.rb@gmail.com>
2025-02-25 15:03:21 +0100

    second

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
- 
-import './App.css';
-
-function App() {
+import { CssBaseline } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+ import {
+   Movies,
+   Actors,
+   MovieInformation,
+   NavBar,
+   Profile,
+ } from "./components";
+ import useStyles from "./styles";
+const App = () => {
+    const classes = useStyles();
   return (
-    <div className="App">
-       
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/" element={<Movies />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-cycle */
+export { default as Actors } from "./Actors/Actors.jsx";
+export { default as MovieInformation } from "./MovieInformation/MovieInformation.jsx";
+export { default as Movies } from "./Movies/Movies.jsx";
+export { default as NavBar } from "./NavBar/NavBar.jsx";
+ 
+export { default as Movie } from "./Movie/Movie.jsx";
+ 
